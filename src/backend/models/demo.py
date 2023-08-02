a7f06fbba500fd5d4b3c5076101051469197c149
Keri Yu <97941468+KeriYuu@users.noreply.github.com>
2023-08-02 21:54:46 +0800

    Update demo.py

--- src/backend/models/demo.py
+++ src/backend/models/demo.py
@@ -28,7 +28,7 @@ if sys.version_info[0] < 3:
 def load_checkpoints(config_path, checkpoint_path, cpu=False):
 
     with open(config_path) as f:
-        config = yaml.load(f)
+        config = yaml.load(f, Loader=yaml.UnsafeLoader)
 
     generator = OcclusionAwareGenerator(**config['model_params']['generator_params'],
                                         **config['model_params']['common_params'])
